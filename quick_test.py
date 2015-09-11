fb5dd8e5afa605d07d140abc8aafef1290a4bbe5
Arkadiusz Tymieniecki <arek@showmyhomework.co.uk>
2015-09-11 16:49:26 +0100

    test all sites,  <-- sample tests

--- quick_test.py
+++ quick_test.py
@@ -3,19 +3,25 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import unittest
 import random
-
+import requests
 subdomain_file='/Users/tymienieckiarkadiusz/repo/salt-stack-beta/haproxy/templates/subdomain_list.txt'
-domain_list = ['showmyhomework.co.uk', 'showmyhomework.com', 'smhwbeta.co.uk', 'smhwfront.co.uk']
+domain_list = ['showmyhomework.com', 'showmyhomework.co.uk', 'smhwfrontend.co.uk',
+               'smhwdev.co.uk', 'smhwbeta.co.uk']
+
 #https://selenium-python.readthedocs.org/getting-started.html#walk-through-of-the-example
 #https://selenium-python.readthedocs.org/locating-elements.html
-
 #suite = unittest.TestSuite()
+
+
 
 class Tests(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Firefox()
+        self.secure = ''
 
+
+    '''
     def test_sidekiqs(self):
         sq_domains = ['www.smhwdev.co.uk', 'showmyhomework.co.uk', 'smhwfront.co.uk'] 
         for domain in sq_domains:
@@ -33,21 +39,41 @@ class Tests(unittest.TestCase):
                     addr = 'http://%(domain)s' % { 'domain': domain }
                     print('sorry dude, cant handle this: %s' % addr)
 
-    def test_subdomains(self):
-        subdomains = [line.split() for line in open(subdomain_file, 'r')][0]
+    '''
+
+
+    def test_all_sites(self):
+        subdomains = [line for line in open(subdomain_file, 'r')]
         random.shuffle(subdomains)
         subdomains = subdomains[:20]
         for domain in domain_list:
-            print('\nlets test random %(sub_nr)s subdomains for %(domain)s' % { 'sub_nr':len(subdomains), 'domain':domain })
+            print('\nlets test random %(sub_nr)s subdomains for %(domain)s' %
+                { 'sub_nr':len(subdomains), 'domain':domain })
             for subd in subdomains:
-                self.test_oops(domain=domain, subdomain=subd)
+                print(subd)
+                self.test_subdomains(domain=domain, subdomain=subd)
 
-    def test_oops(self, domain='smhw.co.uk', subdomain='www'):
-        addr =  'http://%(subdomain)s.%(domain)s' % { 'subdomain': subdomain, 'domain': domain }
+
+    def test_subdomains(self, domain='smhwdev.co.uk', subdomain='www'):
+        addr =  'http%(secure)s://%(subdomain)s.%(domain)s' % {
+             'secure': self.secure, 'subdomain': subdomain, 'domain': domain }
         print(addr)
         self.driver.get(addr)
+        r = requests.get(addr)
         answer = self.driver.page_source
-        self.assertTrue("Oops" or "Oh no" not in answer)
+        self.assertTrue("Oh no" not in answer)
+        self.assertEqual(r.status_code, 200)
+
+
+    def test_domains(self, domain='showmyhomework.com'):
+        addr =  'http%(secure)s://%(domain)s' % {
+                'domain': domain, 'secure': self.secure }
+        print(addr)
+        self.driver.get(addr)
+        r = requests.get(addr)
+        answer = self.driver.page_source
+        self.assertTrue("Oh no" not in answer)
+        self.assertEqual(r.status_code, 200)
 
 
     def tearDown(self):
@@ -57,4 +83,3 @@ class Tests(unittest.TestCase):
 if __name__ == "__main__":
     unittest.main()
     #unittest.TextTestRunner().run(suite)
-    
